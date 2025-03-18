dde78e9d7b9fc138f6e19b3f1b6f7cc95b45e918
DanieleMar09 <danielemarquespessoal@gmail.com>
2025-03-17 21:44:53 -0300

    Validacao de excluir + Tratamento do erro na tela

--- src/components/Usuarios.vue
+++ src/components/Usuarios.vue
@@ -13,6 +13,16 @@
             <i class="fa fa-spinner fa-spin"></i>Carregando....
          </div>
 
+         <div  v-if="erro" class=" alert alert-danger">
+            <i class="fa fa-ban"></i>
+            {{ erro }}
+         </div>
+
+         <div v-if="!carregando && usuarios.length == 0 && !erro" class="alert alert-warning text-center">
+            <i class="fa fa-exclamation-circle">Nenhum usuario cadastrado!</i>
+
+         </div>
+
          <!-- Tabela de listagem -->
           <table  v-if="!carregando" class="table table-striped">
             <thead class="thead-dark">
@@ -39,7 +49,7 @@
                         <i class="fa fa-edit"></i>Editar
                     </button>
 
-                    <button class="btn btn-danger btn-sm ml-2" >
+                    <button class="btn btn-danger btn-sm ml-2" @click="excluir(usuario.id)" >
                         <i class="fa fa-trash"></i>Excluir
                     </button>
                 </td>
@@ -54,11 +64,14 @@
 </template>
 <script>
 import axios from 'axios';
+import Swal from 'sweetalert2';
+
 export default{
     data(){
         return{
             usuarios:[],
             carregando:false,
+            erro:null
         };
     },
     mounted(){
@@ -75,15 +88,42 @@ this.carregarUsuarios();
                     this.usuarios = response.data;
                    this.carregando = false;
 
-                }, 3000)
+                }, 2000)
          
 
             }catch(error){
+                this.erro ="Ocorreu um erro ao listar os usuários"
                 console.log(error);
                 this.carregando =false;
 
             }
 
+        },
+        async excluir(id)
+        {
+            const confirmacao = await Swal.fire({
+                title :"Atenção",
+                text :"Você deseja excluir este usuário",
+                icon :"warning",
+                showCancelButton :true,
+                confirmButtonText :"Sim, desejo excluir",
+                cancelButtonText :"Não"
+
+            });
+
+            if(confirmacao.isConfirmed){
+                try{
+                await axios.delete(`https://localhost:7263/api/v1/usuarios/remover/${id}`);
+                Swal.fire("Excluido!" , "O usuario foi removido com sucesso!" , "sucess");
+          
+                this.carregarUsuarios();
+            }catch(error){
+                Swal.fire("Atenção - Erro!" , "Ocorreu um erro ao excluir o usuário!" , "error");
+               
+            }
+
+            }
+            
         }
 
     }
